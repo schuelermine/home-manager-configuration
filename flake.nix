465e736bb6bed1c5cbb4d52a01bb45df7230ec6b
Anselm Schüler <mail@anselmschueler.com>
2021-11-30 19:38:30 +0100

    Add self inp

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
         home-manager.url = "github:nix-community/home-manager";
         home-manager.inputs.nixpkgs.follows = "system-config/nixos";
     };
-    outputs = { system-config, home-manager }: let nixos = system-config.inputs.nixos;
+    outputs = { system-config, home-manager, self }: let nixos = system-config.inputs.nixos;
     in {
         homeConfigurations.anselmschueler = home-manager.lib.homeManagerConfiguration {
             system = "x86_64-linux";
