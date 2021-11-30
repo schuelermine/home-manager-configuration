a3d656aab8383bca456072558e4335fdb4299d8e
Anselm Schüler <mail@anselmschueler.com>
2021-11-30 19:40:30 +0100

    Add stateVersion to flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
-    inputs = {
-        system-config.url = "git+file:///etc/nixos";
-        home-manager.url = "github:nix-community/home-manager";
-        home-manager.inputs.nixpkgs.follows = "system-config/nixos";
-    };
-    outputs = { system-config, home-manager, self }: let nixos = system-config.inputs.nixos;
+  inputs = {
+    system-config.url = "git+file:///etc/nixos";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "system-config/nixos";
+  };
+  outputs = { system-config, home-manager, self }:
+    let nixos = system-config.inputs.nixos;
     in {
-        homeConfigurations.anselmschueler = home-manager.lib.homeManagerConfiguration {
-            system = "x86_64-linux";
-            homeDirectory = "/home/anselmschueler";
-            username = "anselmschueler";
-            configuration = ./home.nix;
+      homeConfigurations.anselmschueler =
+        home-manager.lib.homeManagerConfiguration {
+          system = "x86_64-linux";
+          homeDirectory = "/home/anselmschueler";
+          username = "anselmschueler";
+          stateVersion = "21.11";
+          configuration = ./home.nix;
         };
     };
 }
