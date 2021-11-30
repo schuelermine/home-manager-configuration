c06f91e20361a2768378dff3b89f5e850ac42c2d
Anselm Schüler <mail@anselmschueler.com>
2021-11-30 19:37:32 +0100

    HM follows nixos

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
     inputs = {
         system-config.url = "git+file:///etc/nixos";
         home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "system-config/nixos";
     };
     outputs = { system-config, home-manager }: let nixos = system-config.inputs.nixos;
     in {
