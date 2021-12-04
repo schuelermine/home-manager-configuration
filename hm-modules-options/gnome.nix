74c64639f9726e8a624f23e8e7938660aa647d8e
Anselm Schüler <mail@anselmschueler.com>
2021-12-04 19:18:50 +0100

    Remove mkMerge

--- hm-modules-options/gnome.nix
+++ hm-modules-options/gnome.nix
@@ -1,15 +1,14 @@
-{ lib, ... }:
-lib.mkMerge [
-  ({ config, pkgs, lib, ... }: {
-    options.gnome.enabledExtensions = lib.mkOption {
-      type = lib.types.listOf lib.types.package;
-      default = [ ];
-      example = [ pkgs.gnomeExtensions.appindicator ];
-      description =
-        "List of packages that provide extensions that are to be enabled";
-    };
-    config.dconf.settings."org/gnome/shell".enabledExtensions =
-      map (pkg: pkg.extensionUuid) config.gnome.enabledExtensions;
-    config.home.packages = config.gnome.enabledExtensions;
-  })
-]
+
+
+{ config, pkgs, lib, ... }: {
+  options.gnome.enabledExtensions = lib.mkOption {
+    type = lib.types.listOf lib.types.package;
+    default = [ ];
+    example = [ pkgs.gnomeExtensions.appindicator ];
+    description =
+      "List of packages that provide extensions that are to be enabled";
+  };
+  config.dconf.settings."org/gnome/shell".enabledExtensions =
+    map (pkg: pkg.extensionUuid) config.gnome.enabledExtensions;
+  config.home.packages = config.gnome.enabledExtensions;
+}
