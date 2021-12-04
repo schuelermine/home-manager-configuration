26a9a2872bb341580c754721d7876a6420aabf6c
Anselm Schüler <mail@anselmschueler.com>
2021-12-04 19:27:49 +0100

    Remove -U from fish color setup code

--- source/colors.fish
+++ source/colors.fish
@@ -1,38 +1,38 @@
-set -U fish_color_autosuggestion brblack
-set -U fish_color_cancel --reverse white
-set -U fish_color_command bryellow
-set -U fish_color_comment brgreen
-set -U fish_color_cwd brgreen
-set -U fish_color_cwd_root brred
-set -U fish_color_end brmagenta
-set -U fish_color_error white
-set -U fish_color_escape yellow
-set -U fish_color_hg_added green
-set -U fish_color_hg_clean green
-set -U fish_color_hg_copied magenta
-set -U fish_color_hg_deleted red
-set -U fish_color_hg_dirty red
-set -U fish_color_hg_modified yellow
-set -U fish_color_hg_renamed magenta
-set -U fish_color_hg_unmerged red
-set -U fish_color_hg_untracked yellow
-set -U fish_color_history_current --underline
-set -U fish_color_host normal
-set -U fish_color_host_remote bryellow
-set -U fish_color_keyword brcyan
-set -U fish_color_match --reverse
-set -U fish_color_normal white
-set -U fish_color_operator brcyan
-set -U fish_color_param brblue
-set -U fish_color_quote brgreen
-set -U fish_color_redirection brmagenta
-set -U fish_color_search_match --reverse
-set -U fish_color_selection --reverse white
-set -U fish_color_status red
-set -U fish_color_user brgreen
-set -U fish_color_valid_path --underline
-set -U fish_pager_color_background black
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description normal
-set -U fish_pager_color_prefix normal
-set -U fish_pager_color_progress --reverse white
+set fish_color_autosuggestion brblack
+set fish_color_cancel --reverse white
+set fish_color_command bryellow
+set fish_color_comment brgreen
+set fish_color_cwd brgreen
+set fish_color_cwd_root brred
+set fish_color_end brmagenta
+set fish_color_error white
+set fish_color_escape yellow
+set fish_color_hg_added green
+set fish_color_hg_clean green
+set fish_color_hg_copied magenta
+set fish_color_hg_deleted red
+set fish_color_hg_dirty red
+set fish_color_hg_modified yellow
+set fish_color_hg_renamed magenta
+set fish_color_hg_unmerged red
+set fish_color_hg_untracked yellow
+set fish_color_history_current --underline
+set fish_color_host normal
+set fish_color_host_remote bryellow
+set fish_color_keyword brcyan
+set fish_color_match --reverse
+set fish_color_normal white
+set fish_color_operator brcyan
+set fish_color_param brblue
+set fish_color_quote brgreen
+set fish_color_redirection brmagenta
+set fish_color_search_match --reverse
+set fish_color_selection --reverse white
+set fish_color_status red
+set fish_color_user brgreen
+set fish_color_valid_path --underline
+set fish_pager_color_background black
+set fish_pager_color_completion normal
+set fish_pager_color_description normal
+set fish_pager_color_prefix normal
+set fish_pager_color_progress --reverse white
