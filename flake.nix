8576a39d9e5e8eb8e48ea2c4d8aaf8570fce2ddb
Anselm Schüler <mail@anselmschueler.com>
2021-12-05 19:21:37 +0100

    Remove unnecessary assignment of nixos

--- flake.nix
+++ flake.nix
@@ -4,17 +4,15 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "system-config/nixos";
   };
-  outputs = { system-config, home-manager, self }:
-    let nixos = system-config.inputs.nixos;
-    in {
-      homeConfigurations.anselmschueler =
-        home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-linux";
-          homeDirectory = "/home/anselmschueler";
-          username = "anselmschueler";
-          stateVersion = "21.11";
-          configuration = ./hm-modules-config/home.nix;
-          extraModules = [ ./hm-modules-options/gnome.nix ];
-        };
-    };
+  outputs = { system-config, home-manager, self }: {
+    homeConfigurations.anselmschueler =
+      home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux";
+        homeDirectory = "/home/anselmschueler";
+        username = "anselmschueler";
+        stateVersion = "21.11";
+        configuration = ./hm-modules-config/home.nix;
+        extraModules = [ ./hm-modules-options/gnome.nix ];
+      };
+  };
 }
