2d99b44509edcbed624c89979191a9bd81c83a9b
Anselm Schüler <mail@anselmschueler.com>
2021-12-09 19:56:29 +0100

    consolidate

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
     system-config.url = "git+file:///etc/nixos";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "system-config/nixos";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "system-config/nixos";
+    };
   };
   outputs = { system-config, home-manager, self }: {
     homeConfigurations.anselmschueler =
