ca9b78d7cf6570d0ac00ffa37641259661f46b60
Anselm Schüler <mail@anselmschueler.com>
2022-01-11 20:22:49 +0100

    &Ŋ

--- source/fish_prompt.fish
+++ source/fish_prompt.fish
@@ -1,4 +1,2 @@
-function fish_prompt
-    set s $status
-    echo -s (test "$s" != 0 && set_color brred || set_color white) $s (set_color white) " " (fish_prompt_pwd_dir_length=0 prompt_pwd) " " \$ (set_color normal) " "
-end
+set s $status
+echo -s (test "$s" != 0 && set_color brred || set_color white) $s (set_color white) " " (fish_prompt_pwd_dir_length=0 prompt_pwd) " " \$ (set_color normal) " "
