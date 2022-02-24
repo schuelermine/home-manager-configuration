d8eca2dafb3cfd4bbeca86ba1873ba6e49a5a0bc
Anselm Schüler <mail@anselmschueler.com>
2022-02-24 20:07:35 +0100

    Use more

--- options/gnome-cursor-theme.nix
+++ options/gnome-cursor-theme.nix
@@ -1,24 +1,13 @@
 { config, pkgs, lib, ... }:
 let cfg = config.gnome.cursorTheme;
-in with lib; {
-  options.gnome.cursorTheme = {
-    package = mkOption {
-      type = types.nullOr types.package;
-      default = null;
-      defaultText = literalExpression "null";
-      example = literalExpression "pkgs.yaru-theme";
-      description = ''
-        Package that provides the cursor theme.
-        If not declared, it's assumed an already installed package provides it.
-      '';
-    };
-    name = mkOption {
-      type = types.str;
-      default = "Adwaita";
-      defaultText = literalExpression ''""'';
-      example = literalExpression ''"Yaru"'';
-      description = "Name of the custom theme.";
-    };
+in with lib // import ../alib.nix lib; {
+  options.gnome.cursorTheme = mkProvidesOption {
+    providedText = "the cursor theme";
+    packageExample = "pkgs.yaru-theme";
+    keyType = types.str;
+    defaultKey = "";
+    defaultKeyText = ''""'';
+    keyExample = ''"Yaru"'';
   };
   config = {
     dconf.settings."org.gnome.desktop.interface".cursor-theme = cfg.name;
