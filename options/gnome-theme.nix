3c9895797edf6c5fe631d59ba1433c677cd03817
Anselm Schüler <mail@anselmschueler.com>
2022-02-25 19:05:45 +0100

    foobar

--- options/gnome-theme.nix
+++ options/gnome-theme.nix
@@ -1,19 +1,22 @@
 { config, pkgs, lib, ... }:
 with lib // import ../alib.nix lib; {
+  imports = [
+    (mkProvidesModule {
+      providedText = "the custom shell theme";
+      packageExample = "pkgs.yaru-theme";
+      keyType = types.str;
+      defaultKey = "";
+      defaultKeyText = ''""'';
+      keyExample = ''"Yaru"'';
+      prefix = [ "gnome" "shellTheme" ];
+    })
+  ];
   options.gnome.shellTheme = {
     enable = mkEnableOption "custom GNOME shell themes";
-  } // mkProvidesOptionSet {
-    providedText = "the custom shell theme";
-    packageExample = "pkgs.yaru-theme";
-    keyType = types.str;
-    defaultKey = "";
-    defaultKeyText = ''""'';
-    keyExample = ''"Yaru"'';
   };
   config = let cfg = config.gnome.shellTheme;
   in mkIf cfg.enable {
     gnome.extensions.enabledExtensions = [ pkgs.gnomeExtensions.user-themes ];
     dconf.settings."org/gnome/shell/extensions/user-theme".name = cfg.name;
-    home.packages = optional (cfg.package != null) cfg.package;
   };
 }
