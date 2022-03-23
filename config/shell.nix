617343b121c7f0f9943d6d54c7ba7a3c156262b4
Anselm Schüler <mail@anselmschueler.com>
2022-03-23 18:57:32 +0100

    + +#-U	modified:   config/shell.nix

--- config/shell.nix
+++ config/shell.nix
@@ -44,7 +44,7 @@ in {
       shellInit = builtins.concatStringsSep "\n" [
         (builtins.readFile ../source/colors.fish)
         ''
-          set fish_features stderr-nocaret qmark-noglob regex-easyesc ampersand-nobg-in-token
+          set -U fish_features stderr-nocaret qmark-noglob regex-easyesc ampersand-nobg-in-token
         ''
       ];
     };
