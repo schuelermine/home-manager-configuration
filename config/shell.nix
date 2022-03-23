8c2f45213af03d101556af4f35f5d132123865fc
Anselm Schüler <mail@anselmschueler.com>
2022-03-23 18:54:54 +0100

    .#Fix error

--- config/shell.nix
+++ config/shell.nix
@@ -41,7 +41,7 @@ in {
         else
           { }) (nix-lib.file.readDirRCollapsed "${fish-functions}");
       prompt = builtins.readFile ../source/fish_prompt.fish;
-      shellInit = concatStringSep "\n" [
+      shellInit = builtins.concatStringSep "\n" [
         (builtins.readFile ../source/colors.fish)
         ''
           set fish_features stderr-nocaret qmark-noglob regex-easyesc ampersand-nobg-in-token
