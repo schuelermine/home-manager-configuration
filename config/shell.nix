59e487289e7c6218893ff9b4a698df2abe26d1f6
Anselm Schüler <mail@anselmschueler.com>
2022-03-23 18:54:19 +0100

    Add experimental fish features#	modified:   config/shell.nix

--- config/shell.nix
+++ config/shell.nix
@@ -41,7 +41,12 @@ in {
         else
           { }) (nix-lib.file.readDirRCollapsed "${fish-functions}");
       prompt = builtins.readFile ../source/fish_prompt.fish;
-      shellInit = builtins.readFile ../source/colors.fish;
+      shellInit = concatStringSep "\n" [
+        (builtins.readFile ../source/colors.fish)
+        ''
+          set fish_features stderr-nocaret qmark-noglob regex-easyesc ampersand-nobg-in-token
+        ''
+      ];
     };
   };
   home = {
