27b45934ea4a6dd3b43e3e6a0af8ca518ffb0e58
Anselm Schüler <mail@anselmschueler.com>
2022-04-01 19:23:02 +0200

    refactor gnome-cursor-theme

--- options/gnome-cursor-theme.nix
+++ options/gnome-cursor-theme.nix
@@ -1,16 +1,18 @@
 { config, pkgs, lib, ... }:
 let cfg = config.gnome.cursorTheme;
 in with lib // import ../alib.nix lib; {
-  options.gnome.cursorTheme = mkProvidesOption {
-    providedText = "the cursor theme";
-    packageExample = "pkgs.yaru-theme";
-    keyType = types.str;
-    defaultKey = "";
-    defaultKeyText = ''""'';
-    keyExample = ''"Yaru"'';
-  };
-  config = {
-    dconf.settings."org/gnome/desktop/interface".cursor-theme = cfg.name;
-    home.packages = optional (cfg.package != null) cfg.package;
-  };
+  imports = [
+    (mkProvidesModule {
+      providedText = "the cursor theme";
+      packageExample = ''"pkgs.yaru-theme"'';
+      keyType = types.str;
+      defaultKey = "Adwaita";
+      defaultKeyText = ''"Adwaita"'';
+      keyExample = ''"Yaru"'';
+      prefix = [ "options" "gnome" "cursorTheme" ];
+    })
+  ];
+  config.dconf.settings."org/gnome/desktop/interface".cursor-theme =
+    mkIf (cfg != null && cfg.name != null && cfg.name != "")
+    cfg.name; # In case the Adwaita default is changed
 }
