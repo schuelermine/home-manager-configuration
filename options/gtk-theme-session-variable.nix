e4d0590eeb12d515a2bddce249334c09ed9267ce
Anselm Schüler <mail@anselmschueler.com>
2022-04-19 14:26:04 +0200

    Fix missing qualifier

--- options/gtk-theme-session-variable.nix
+++ options/gtk-theme-session-variable.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: {
-  config = mkIf (config.gtk.enable
+  config = lib.mkIf (config.gtk.enable
     && !builtins.elem config.gtk.theme.name or null [ null "" ]) {
       home.sessionVariables.GTK_THEME = config.gtk.theme.name;
     };
