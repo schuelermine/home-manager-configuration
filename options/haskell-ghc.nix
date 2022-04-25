588a6a68f75870a80e6cf07708a56c4e82579bb4
Anselm Schüler <mail@anselmschueler.com>
2022-04-25 19:26:19 +0200

    Fix warning outside config

--- options/haskell-ghc.nix
+++ options/haskell-ghc.nix
@@ -22,14 +22,16 @@ in {
       example = literalExpression "hkgs: [ hkgs.primes ]";
     };
   };
-  config.home.packages = mkIf cfg.enable [
-    (if pkgsConfigurable then
-      cfg.package.withPackages cfg.packages
-    else
-      cfg.package)
-  ];
-  warnings = mkIf (cfg.enable && pkgsConfigurable) [''
-    You have provided a package as programs.haskell.ghc.package that doesn't have the withPackages utility function.
-    This disables specifying packages via programs.haskell.ghc.packages.
-  ''];
+  config = {
+    home.packages = mkIf cfg.enable [
+      (if pkgsConfigurable then
+        cfg.package.withPackages cfg.packages
+      else
+        cfg.package)
+    ];
+    warnings = mkIf (cfg.enable && pkgsConfigurable) [''
+      You have provided a package as programs.haskell.ghc.package that doesn't have the withPackages utility function.
+      This disables specifying packages via programs.haskell.ghc.packages.
+    ''];
+  };
 }
