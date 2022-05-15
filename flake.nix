8eba6788503f54f4095f92ff037e03ff3e385507
Anselm Schüler <mail@anselmschueler.com>
2022-05-15 14:55:35 +0200

    Fix wrong &?

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
     tetris.url = "github:schuelermine/tetris/add-nix-build";
     blender = {
-      url = "github:schuelermine/nix-warez/blender-improved&dir=blender";
+      url = "github:schuelermine/nix-warez/blender-improved?dir=blender";
       inputs.nixpkgs.follows = "system-config/nixpkgs";
     };
   };
