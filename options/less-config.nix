aa0f0403c3479dea388b239c03a7d563ff85ea54
Anselm Schüler <mail@anselmschueler.com>
2022-06-02 21:34:22 +0200

    rm dup

---
+++ options/less-config.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+with lib;
+let cfg = config.programs.less;
+in {
+  options.programs.less.options = mkOption {
+    type = types.listOf types.str;
+    description = ''
+      The options passed to less. This sets the <code>$LESS</code> environment variable.
+    '';
+    default = [ ];
+    example = ''[ "-R" ]'';
+  };
+  config.home = mkIf cfg.enable {
+    sessionVariables = {
+      "LESS" = builtins.concatStringsSep " " cfg.options;
+      "PAGER" = "${cfg.package}";
+    };
+  };
+}
