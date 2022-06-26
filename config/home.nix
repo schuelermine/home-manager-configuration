522a3aef50e2f09be54d8d881b49f644ae91fd69
Anselm Schüler <mail@anselmschueler.com>
2022-06-26 22:30:42 +0200

    Fix issue

--- config/home.nix
+++ config/home.nix
@@ -1,7 +1,9 @@
 { config, pkgs, lib, ... }: {
   nixpkgs.config.allowUnfreePredicate = (_: true);
   programs.home-manager.enable = true;
-  homeDirectory = "/home/anselmschueler";
-  username = "anselmschueler";
-  stateVersion = "21.11";
+  home = {
+    homeDirectory = "/home/anselmschueler";
+    username = "anselmschueler";
+    stateVersion = "21.11";
+  };
 }
