fee61e81c316b59db02a95d352285ca151797bf1
Anselm Schüler <mail@anselmschueler.com>
2022-09-18 21:12:56 +0200

    Fob

--- options/config-variables.nix
+++ options/config-variables.nix
@@ -1,42 +1,48 @@
 { pkgs, lib, lib1, ... }:
 with builtins // lib // lib1;
 let
-  mkModule = { name, varName ? toUpper (head (split " " name))
-    , optionName ? snakeCase name }:
+  mkModule =
+    { name
+    , varName ? toUpper (head (split " " name))
+    , optionName ? snakeCase name
+    , example ? "nano"
+    }:
     { config, ... }:
-    with types;
-    let
-      cfg = config.home.${optionName};
-      moduleType = mkProvidesType {
-        providedText = "the ${name}";
-        packageExample = "pkgs.nano";
-        keyName = "executable";
-        keyType = nullOr (oneOf [ str path ]);
-        defaultKey = null;
-        defaultKeyText = "null";
-        keyExample = ''"nano"'';
-      };
-    in {
-      options.home.${optionName} = mkOption {
-        type = nullOr (oneOf [ str moduleType ]);
-        default = null;
-        description = ''
-          The ${name} to use. This sets the <code>${varName}</code> variable.
-          Can be a string or a submodule specifying a <code>package</code> and an <code>executable</code>.
-        '';
-      };
-      config.home = {
-        packages = mkIf (isAttrs cfg && cfg.package != null) [ cfg.package ];
-        sessionVariables = {
-          ${guardNull cfg varName} =
-            if isString cfg then cfg else toString cfg.executable;
+      with types;
+      let
+        cfg = config.home.${optionName};
+        moduleType = mkProvidesType {
+          providedText = "the ${name}";
+          packageExample = "pkgs.nano";
+          keyName = "executable";
+          keyType = nullOr (oneOf [ str path ]);
+          defaultKey = null;
+          defaultKeyText = "null";
+          keyExample = ''"${example}"'';
+        };
+      in
+      {
+        options.home.${optionName} = mkOption {
+          type = nullOr (oneOf [ str moduleType ]);
+          default = null;
+          description = ''
+            The ${name} to use. This sets the <code>${varName}</code> variable.
+            Can be a string or a submodule specifying a <code>package</code> and an <code>executable</code>.
+          '';
+        };
+        config.home = {
+          packages = mkIf (isAttrs cfg && cfg.package != null) [ cfg.package ];
+          sessionVariables = {
+            ${guardNull cfg varName} =
+              if isString cfg then cfg else toString cfg.executable;
+          };
         };
       };
-    };
-in {
+in
+{
   imports = map mkModule [
     { name = "editor"; }
     { name = "visual editor"; }
-    { name = "pager"; }
+    { name = "pager"; example = "more"; }
   ];
 }
