255726b60f89cbb48e27d9ebf08ecfe9c8e2a6d1
Anselm Schüler <mail@anselmschueler.com>
2022-10-21 20:58:32 +0200

    Revert "switch to vscodium"
    This reverts commit eb681f507425802c4480c62d8d24f3ed200cb989.

--- config/vscode.nix
+++ config/vscode.nix
@@ -1,7 +1,6 @@
 { config, pkgs, lib, ... }: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
     mutableExtensionsDir = false;
     extensions = with pkgs.vscode-extensions; [
       editorconfig.editorconfig
