af513404e7d0ddaddf1811d84275510d8ab0c6ff
Anselm Schüler <mail@anselmschueler.com>
2022-10-23 02:18:37 +0200

    vscode: more vscode stettings

--- config/vscode-rust.nix
+++ config/vscode-rust.nix
@@ -2,4 +2,9 @@
   programs.vscode.extensions = with pkgs.vscode-extensions; [
     rust-lang.rust-analyzer
   ];
+  userSettings = {
+    "[rust]"."editor.formatOnSave" = true;
+    "rust-analyzer.server.path" =
+      "${config.programs.rust.rust-analyzer.package}/bin/rust-analyzer";
+  };
 }
