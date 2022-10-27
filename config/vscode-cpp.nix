4f5772be9086113c2d62e742fac231a3379af40b
Anselm Schüler <mail@anselmschueler.com>
2022-10-28 00:01:56 +0200

    vscode: set clang_format_path & add firefox dbeuggs

--- config/vscode-cpp.nix
+++ config/vscode-cpp.nix
@@ -5,6 +5,7 @@
     ];
     userSettings = {
       "C_Cpp.formatting" = "clangFormat";
+      "C_Cpp.clang_format_path" = "${pkgs.clang-tools}/bin/clang-format";
       "cmake.cmakePath" = "${pkgs.cmake}/bin/cmake";
       "makefile.makePath" = "${pkgs.gnumake}/bin/make";
     };
