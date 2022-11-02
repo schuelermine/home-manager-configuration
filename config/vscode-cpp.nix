027ae2ec4f2948e70fb8dca184ef514e5fad8058
Anselm Schüler <mail@anselmschueler.com>
2022-11-02 15:27:01 +0100

    Add C/C++ extension from MS

--- config/vscode-cpp.nix
+++ config/vscode-cpp.nix
@@ -2,9 +2,18 @@
   programs.vscode = {
     extensions = with pkgs.vscode-extensions; [
       llvm-vs-code-extensions.vscode-clangd
+      ms-vscode.cpptools
+      twxs.cmake
+      ms-vscode.makefile-tools
     ];
     userSettings = {
       "clangd.path" = "${pkgs.clang-tools}/bin/clangd";
+      "cmake.cmakePath" = "${pkgs.cmake}/bin/cmake";
+      "makefile.makePath" = "${pkgs.gnumake}/bin/make";
+      "C_Cpp.autocomplete" = "Disabled";
+      "C_Cpp.formatting" = "Disabled";
+      "C_Cpp.errorSquiggles" = "Disabled";
+      "C_Cpp.intelliSenseEngine" = "Disabled";
     };
   };
 }
