479d02568f1a515a475b5c823abd5980ee305442
Anselm Schüler <mail@anselmschueler.com>
2022-11-02 23:37:52 +0100

    vscode: move away from MS C/C++ extension

--- config/vscode-cpp.nix
+++ config/vscode-cpp.nix
@@ -3,18 +3,14 @@
     extensions = with pkgs.vscode-extensions; [
       llvm-vs-code-extensions.vscode-clangd
       ms-vscode.cmake-tools
-      ms-vscode.cpptools
       ms-vscode.makefile-tools
       twxs.cmake
+      vadimcn.vscode-lldb
     ];
     userSettings = {
       "clangd.path" = "${pkgs.clang-tools}/bin/clangd";
       "cmake.cmakePath" = "${pkgs.cmake}/bin/cmake";
       "makefile.makePath" = "${pkgs.gnumake}/bin/make";
-      "C_Cpp.autocomplete" = "Disabled";
-      "C_Cpp.formatting" = "Disabled";
-      "C_Cpp.errorSquiggles" = "Disabled";
-      "C_Cpp.intelliSenseEngine" = "Disabled";
     };
   };
 }
