be28c74fb8ed3470c1066d8a0c94c8c73f523c3b
Anselm Schüler <mail@anselmschueler.com>
2022-11-08 00:26:39 +0100

    bar

--- config/vscode-haskell.nix
+++ config/vscode-haskell.nix
@@ -1,13 +1,14 @@
 { config, pkgs, lib, ... }: {
-  programs.vscode = {
-    extensions = with pkgs.vscode-extensions; [
-      haskell.haskell
-      justusadam.language-haskell
-    ];
-    userSettings = {
-      "haskell.serverExecutablePath" =
-        "${config.programs.haskell.language-server.package}/bin/haskell-language-server-wrapper";
+  programs = {
+    vscode = {
+      extensions = with pkgs.vscode-extensions; [
+        haskell.haskell
+        justusadam.language-haskell
+      ];
+      userSettings = {
+        "haskell.serverExecutablePath" =
+          "${config.programs.haskell.language-server.package}/bin/haskell-language-server-wrapper";
+      };
     };
   };
-  programs.haskell.language-server.enable = true;
 }
--- options/haskell-language-server.nix
+++ options/haskell-language-server.nix
@@ -2,18 +2,18 @@
 with builtins // lib;
 let cfg = config.programs.haskell.language-server;
     ghcVersionName = config.programs.haskell.ghcVersionName;
-    pkgConfigurable =
-      cfg.package.override.__functionArgs ? supportedGhcVersions;
-    pkgConfigured = if pkgConfigurable && ghcVersionName != null
-      then cfg.package.override {
-        supportedGhcVersions = [ ghcVersionName ] ++ cfg.extraSupportedGhcVersions;
-      }
-      else cfg.package;
 in {
   options.programs.haskell.language-server = {
     enable = mkEnableOption "the Haskell Language Server";
     package = mkPackageOption pkgs "HLS" {
       default = [ "haskell-language-server" ];
+    } // {
+      apply = pkg:
+        if pkg.override.__functionArgs ? supportedGhcVersions && ghcVersionName != null
+        then pkg.override {
+          supportedGhcVersions = [ ghcVersionName ] ++ cfg.extraSupportedGhcVersions;
+        }
+        else pkg;
     };
     extraSupportedGhcVersions = mkOption {
       type = with types; listOf str;
@@ -24,8 +24,8 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    home.packages = [ pkgConfigured ];
-    warnings = mkIf (!pkgConfigurable) [''
+    home.packages = [ cfg.package ];
+    warnings = mkIf (!cfg.package.override.__functionArgs ? supportedGhcVersions) [''
       You have provided a package as programs.haskell.language-server.package that doesn't allow overriding supportedGhcPackages.
       This disables specifying supported GHC versions via programs.haskell.language-server.extraSupportedGhcVersions.
       It is recommended to use pkgs.haskell-language-server or a derivative, not a HLS from haskellPackages or similar.
