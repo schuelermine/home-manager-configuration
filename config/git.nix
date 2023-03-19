a007392afdc7e956cc549dfd902d6d71f42924f4
Anselm Schüler <mail@anselmschueler.com>
2023-03-19 22:54:21 +0100

    Add ownership exception for /etc/nixos for git

--- config/git.nix
+++ config/git.nix
@@ -6,7 +6,12 @@
       enable = true;
       delta.enable = true;
       package = pkgs.gitFull;
-      extraConfig.init.defaultBranch = "b0";
+      extraConfig = {
+        init.defaultBranch = "b0";
+        safe.directory = [
+          "/etc/nixos"
+        ];
+      };
     };
     gh = {
       enable = true;
