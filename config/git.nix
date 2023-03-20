a62bb1f86447fa2982acd6d5baa5880de8ee8d69
Anselm Schüler <mail@anselmschueler.com>
2023-03-20 12:31:06 +0100

    Revert "Add ownership exception for /etc/nixos for git"
    This reverts commit a007392afdc7e956cc549dfd902d6d71f42924f4.

--- config/git.nix
+++ config/git.nix
@@ -6,12 +6,7 @@
       enable = true;
       delta.enable = true;
       package = pkgs.gitFull;
-      extraConfig = {
-        init.defaultBranch = "b0";
-        safe.directory = [
-          "/etc/nixos"
-        ];
-      };
+      extraConfig.init.defaultBranch = "b0";
     };
     gh = {
       enable = true;
